8598db9e9bc750905e27180b89039ddb9f5d8b22
Panagiotis Dimopoulos <panosdim@gmail.com>
2025-04-02 21:42:33 +0300

    Update dependencies and version
    - Updates dependencies, including Kotlin, Gradle, Compose, and Firebase.
    - Updates the version code and name.
    - Updates the minimum fetch interval for remote config.
    - Adds remote config to get the update URL.

--- app/src/main/java/com/panosdim/carmaintenance/MainActivity.kt
+++ app/src/main/java/com/panosdim/carmaintenance/MainActivity.kt
@@ -9,6 +9,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -23,6 +24,8 @@ import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.google.firebase.FirebaseApp
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import com.panosdim.carmaintenance.ui.MainScreen
 import com.panosdim.carmaintenance.ui.theme.CarMaintenanceTheme
 import com.panosdim.carmaintenance.utils.checkForNewVersion
@@ -35,6 +38,7 @@ import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
     private lateinit var manager: DownloadManager
+    private lateinit var remoteConfig: FirebaseRemoteConfig
     private val scope = CoroutineScope(Dispatchers.IO)
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -81,10 +85,25 @@ class MainActivity : ComponentActivity() {
         createNotificationChannel(this)
         FirebaseApp.initializeApp(this)
 
-        // Check for new version
-        scope.launch {
-            checkForNewVersion(this@MainActivity)
-        }
+        remoteConfig = FirebaseRemoteConfig.getInstance()
+        val configSettings = FirebaseRemoteConfigSettings.Builder()
+            .setMinimumFetchIntervalInSeconds(2592000) // Fetch at least every 30 days
+            .build()
+        remoteConfig.setConfigSettingsAsync(configSettings)
+
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val updateUrl = remoteConfig.getString("UPDATE_URL")
+                    // Check for new version
+                    scope.launch {
+                        checkForNewVersion(this@MainActivity, updateUrl)
+                    }
+                } else {
+                    // Handle fetch failure (e.g., log the error)
+                    Log.e(TAG, "Error fetching remote config", task.exception)
+                }
+            }
 
         // Check for Notifications Permissions
         if (!NotificationManagerCompat.from(this).areNotificationsEnabled()) {
